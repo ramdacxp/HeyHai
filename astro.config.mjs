d79d884920fdf63c23f611cec18f666f86b8e858
Michael Hoser <michael.hoser@outlook.com>
2025-04-26 00:48:49 +0200

    LoadData first part

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ import alpinejs from '@astrojs/alpinejs';
 // https://astro.build/config
 export default defineConfig({
   outDir: 'dist/www',
+  site: 'https://schademarmelade.de',
+  // base: 'root/',
 
   vite: {
     plugins: [tailwindcss()]

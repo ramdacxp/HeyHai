1c74bf4e9d7503db2f0ac6958e75cda3835e34c1
Michael Hoser <michael.hoser@outlook.com>
2025-04-27 00:03:34 +0200

    Use Prod or dev

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,7 @@
-export const API_URL = `${import.meta.env.SITE}${import.meta.env.BASE_URL}api/`;
+// fake and real uris
+export const FAKE_API_URL = `${import.meta.env.BASE_URL}api/sampledata.json`;
+export const PROD_API_URL = `${import.meta.env.SITE}${import.meta.env.BASE_URL}api/jokes`;
 
-export const TESTAPI_URL = `${import.meta.env.BASE_URL}api/sampledata.json`;
-
-// export const REMOTE_ASSETS_BASE_URL = `https://flowbite-admin-dashboard.vercel.app`;
-
-// use fake data
-export const RANDOMIZE = Boolean(import.meta.env.RANDOMIZE) || true;
+// choose based on environment (dev: npm run dev, prod: npm run build)
+export const ISPROD = import.meta.env.MODE === "production";
+export const API_URL = ISPROD ? PROD_API_URL : FAKE_API_URL;
